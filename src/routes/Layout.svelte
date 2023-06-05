180668b60e3ea4ffc43620a88d299b8b41d4de2e
Suprada-2002 <supradatripathy789@gmail.com>
2023-06-05 13:07:46 +0530

    adding layout

---
+++ src/routes/Layout.svelte
@@ -0,0 +1,20 @@
+<nav>
+    <ul>THolyV</ul>
+    <ul>
+        <li><a href="/">contributors</a></li>
+        <li><a href="/">contact</a></li>
+    </ul>
+</nav>
+
+<slot />
+
+<style>
+    nav{
+        background-color: rgb(130, 133, 133);
+    }
+    ul{
+        display: grid;
+        place-items: center;
+        grid-template-columns: 1fr 1fr;
+    }
+</style>
