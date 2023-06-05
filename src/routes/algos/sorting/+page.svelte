858f026224948d02080d604e1dc0f89f202af7da
AyushmanTripathy <ayushmantripathy2004@gmail.com>
2023-06-05 22:52:25 +0530

    added merge sort and button logic for /sorting

--- src/routes/algos/sorting/+page.svelte
+++ src/routes/algos/sorting/+page.svelte
@@ -4,8 +4,22 @@
   import alogrithms, { content } from "./algorithm.js";
 
   let name = $page.url.searchParams.get("q");
+  console.log($page);
   const algo = alogrithms[name];
   if (typeof algo == "undefined") console.error("invalid name of algorithim");
+
+  let timeOutId;
+  let buttonString = "Slow Down";
+  let buttonSecString = "Speed Up";
+  let resolveFunction;
+
+
+  // button logic
+  let delayTime = 500;
+  let paramsDelay = $page.url.searchParams.get("delay")
+  let handleClick = () => {};
+  let handleSecClick = () => {};
+
 
   // first letter to uppercase
   const description = content[name];
@@ -15,17 +29,65 @@
 
   let bars = [];
   onMount(() => {
+    const handleIncrease= () => {
+      if (delayTime <= 100) buttonSecString = "Maximum Speed";
+      else delayTime = delayTime - 100;
+    }
+    const handleReset = () => {
+      delayTime = 500;
+      buttonString = "Slow Down"
+      buttonSecString = "Speed Up"
+      handleClick = handleSlowDown;
+      handleSecClick = handleIncrease;
+      handleNext();
+    }
+    const handleNext = () => {
+      clearTimeout(timeOutId);
+      resolveFunction();
+    }
+    const handleSlowDown = () => {
+      buttonString = "Next Step"
+      buttonSecString = "Reset"
+      handleClick = handleNext;
+      delayTime = 10000;
+      handleSecClick = handleReset;
+    }
+
+    if (paramsDelay) {
+      delayTime = paramsDelay
+      if (delayTime >= 10000) {
+        buttonString = "Next Step";
+        buttonSecString = "Reset";
+        handleClick = handleNext;
+        handleSecClick = handleReset;
+      } else handleClick = handleSlowDown;
+    } else {
+      handleClick = handleSlowDown;
+      handleSecClick = handleIncrease;
+    }
+
     const n = Math.floor(window.innerWidth / 70);
     bars = initRandomBars(n);
-    const delay = ms => new Promise(res => setTimeout(res, ms));
+    const delay = (ms) => {
+      return new Promise(res => {
+          resolveFunction = res;
+          timeOutId = setTimeout(res, ms)
+        });
+    }
     const update = async (newBars) => { 
       bars = newBars;
-      await delay(500);
+      await delay(delayTime);
       for (const bar of bars) bar.hl = 0;
     }
-    const end = () => console.log("Finished");
-
-    algo(bars, update, end, 500);
+    const end = () => {
+      console.log("Finished") 
+      buttonString = "Restart"
+      handleClick = () => {
+        window.location.href
+        = `${$page.url.pathname}?q=${name.toLowerCase()}&delay=${delayTime}`;
+      }
+    }
+    algo(bars, update, end);
   });
 
   function initRandomBars(n) {
@@ -49,6 +111,8 @@
       {scenerio} case: O({complexity[0]}) comparision and O({complexity[1]}) swaps<br>
     {/each}
   </p>
+  <button on:click={handleClick}> {buttonString} </button>
+  <button on:click={handleSecClick}> {buttonSecString} </button>
 </section>
 <section id="visuals">
   {#each bars as {height, number, hl}}
@@ -77,7 +141,10 @@
     width: 5vw;
     max-width: 40px;
     border-radius: 2%;
-    border: 4px solid $tri;
+    border: 4px solid #000;
+  }
+  .hl-1 {
+    border-color: transparent;
   }
   .hl1 {
     border-color: $hl;
--- src/routes/algos/sorting/algorithm.js
+++ src/routes/algos/sorting/algorithm.js
@@ -1,4 +1,4 @@
-async function bubble(bars, update, end, delay) {
+async function bubble(bars, update, end) {
   let swapped = true;
   while (swapped) {
     swapped = false;
@@ -16,10 +16,10 @@ async function bubble(bars, update, end, delay) {
       }
     }
   }
-  end();
+  await end();
 }
 
-async function selection(bars, update, end, delay) {
+async function selection(bars, update, end) {
   for (let i = 0; i < bars.length; i++) {
     let minValue = bars[i].number;
     let minIndex = i;
@@ -41,6 +41,73 @@ async function selection(bars, update, end, delay) {
     bars[minIndex] = temp;
     await update(bars);
   }
+  await end();
+}
+
+function markUnFocused(bars, start, end) {
+  for (let i = 0; i < bars.length; i++) {
+    if (i < start || end < i) bars[i].hl = -1;
+  }
+}
+
+async function mergeSort(bars, start, end, update) {
+  bars[start].hl = 1;
+  bars[end - 1].hl = 1;
+  await update(bars);
+  if (end - start == 1) return bars;
+
+  const divider = Math.floor((end - start) / 2) + start;
+  const left = await mergeSort(bars, start, divider, update);
+  const right = await mergeSort(bars, divider, end, update);
+
+  let leftIndex = start;
+  let rightIndex = divider;
+  const newBars = [];
+
+  // merging
+  while (true) {
+    bars[leftIndex].hl = 2;
+    bars[rightIndex].hl = 2;
+    markUnFocused(bars, start, end);
+    await update(bars);
+    if (left[leftIndex].number < right[rightIndex].number) {
+      newBars.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      newBars.push(right[rightIndex]);
+      rightIndex++;
+    }
+    if (leftIndex == divider || rightIndex == end) break;
+  }
+
+  // adding leftovers
+  if (leftIndex != divider) {
+    for (; leftIndex < divider; leftIndex++) {
+      bars[leftIndex].hl = 2;
+      markUnFocused(bars, start, end)
+      await update(bars);
+      newBars.push(left[leftIndex]);
+    }
+  } else if (rightIndex != end) {
+    for (; rightIndex < end; rightIndex++) {
+      bars[leftIndex].hl = 2;
+      markUnFocused(bars, start, end)
+      await update(bars);
+      newBars.push(right[rightIndex]);
+    }
+  }
+
+  //copying
+  for (let i = 0; i < newBars.length; i++) bars[start + i] = newBars[i];
+  markUnFocused(bars, start, end);
+  await update(bars);
+  return bars;
+}
+
+async function merge(bars, update, end, delay) {
+  await mergeSort(bars, 0, bars.length, update);
+  await update(bars);
+  await end();
 }
 
 export const content = {
@@ -56,6 +123,11 @@ export const content = {
       worst: ["n^2", "n"],
     },
   },
+  merge: {
+    time: {
+      worst: ["n - 1", "?"],
+    },
+  },
 };
 
-export default { bubble, selection };
+export default { bubble, selection, merge };
