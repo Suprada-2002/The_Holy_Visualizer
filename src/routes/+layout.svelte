68bd7c5e39f11daa9759dd7d94da3a0705716ff1
Suprada-2002 <supradatripathy789@gmail.com>
2023-06-05 13:29:23 +0530

    changing layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,12 @@
+<script>
+    import "../style.css"
+    </script>
+
 <nav>
-    <ul>THolyV</ul>
+    <ul><a>THolyV</a></ul>
     <ul>
-        <li><a href="/">contributors</a></li>
-        <li><a href="/">contact</a></li>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About Us</a></li>
     </ul>
 </nav>
 
@@ -11,10 +15,19 @@
 <style>
     nav{
         background-color: rgb(130, 133, 133);
-    }
-    ul{
         display: grid;
         place-items: center;
         grid-template-columns: 1fr 1fr;
+        padding: 7px 0px;
+    }
+    ul{
+       display: flex;  
+    }
+    li{
+        list-style: none;
+        margin-right: 10px;
+    }
+    a{
+        text-decoration: none;
     }
 </style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,1 @@
+<h1>contributors page</h1>
